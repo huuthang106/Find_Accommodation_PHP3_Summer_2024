77d6b1dac5b60228c537169223bb1919e6c3a878
MHuy12 <145202827+MHuy12@users.noreply.github.com>
2024-07-17 22:56:38 +0700

    [Back_end]_Dangnhap_nguoidung

---
+++ Find_Accommodation_PHP3_Summer_2024/app/Http/Controllers/Client/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function pages_login()
+    {
+        return view('layouts.layout-user');
+    }
+    public function check_login()
+    {
+        // Bắt lỗi
+        request()->validate([
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required',
+        ], [
+            'email.exists' => 'Email không tồn tại trong hệ thống.',
+            'email.required' => 'Vui lòng nhập địa chỉ email.',
+            'email.email' => 'Địa chỉ email không hợp lệ.',
+            'password.required' => 'Vui lòng nhập mật khẩu.',
+        ]);
+
+        $credentials = request()->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            // Authentication passed
+          dd(auth());
+            return redirect()->route('home');
+        }
+
+        // Authentication failed
+        return redirect()->back()->withErrors([
+            'password' => 'Mật khẩu không chính xác.',
+        ]);
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
