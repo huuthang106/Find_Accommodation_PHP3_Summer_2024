a4bd3e624c76aa08375bac43430b51804eaeee39
huuthang106 <thangcachep106@gmail.com>
2024-08-07 13:53:14 +0700

    [fix]-loi-xoa-don

--- Find_Accommodation_PHP3_Summer_2024/resources/views/admincp/manages/pages-notification.blade.php
+++ Find_Accommodation_PHP3_Summer_2024/resources/views/admincp/manages/pages-notification.blade.php
@@ -20,17 +20,11 @@
                         <table id="datatable" class="table table-bordered dt-responsive nowrap">
                             <thead>
                                 <tr>
-                                    <th style="min-width: 95px;">
-                                        <div class="checkbox checkbox-single checkbox-primary">
-                                            Tất cả
-                                            <input type="checkbox" class="custom-control-input" id="action-checkbox">
-                                            <label class="custom-control-label" for="action-checkbox">&nbsp;</label>
-                                        </div>
-                                    </th>
+                                  
                                     <th>Loại</th>
-                                    <th>Dữ liệu</th>
-                                    <th>Nội dung</th>
-                                    <th>Trạng thái</th>
+                               
+                                  
+                                   
                                     <th>Ngày</th>
                                     <th>Thao tác</th>
                                 </tr>
@@ -38,16 +32,12 @@
                             <tbody>
                                 @foreach ($notification as $item)
                                     <tr>
-                                        <td>
-                                            <div class="checkbox checkbox-primary mr-2 float-left">
-                                                <input id="checkbox{{ $item->id }}" type="checkbox">
-                                                <label for="checkbox{{ $item->id }}"></label>
-                                            </div>
+                                
+                                        <td>{{ $item->type }}
+                                       <br><small> {{ $item->message }}</small> <br><small> {{ $item->status == 1 ? 'Chưa xem' : 'Đã xem' }}</small>
                                         </td>
-                                        <td>{{ $item->type_limit }}</td>
-                                        <td>{{ $item->data_limit }}</td>
-                                        <td>{{ $item->message_limit }}</td>
-                                        <td>{{ $item->status == 1 ? 'Chưa xem' : 'Đã xem' }}</td>
+                                     
+                                    
                                         <td>{{ $item->created_at->format('d/m/Y H:i:s') }}</td>
                                         <td>
                                             {{-- <a href="{{ route('admin.notification.view.detail', ['id' => $item->id]) }}"
