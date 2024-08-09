8693dcfd621ac7610b8fa43efb7bd289cadba3b8
Bun194 <0109duynguyen@gmail.com>
2024-08-09 14:14:16 +0700

    [FIX]_TrangChiTietTinDang

--- Find_Accommodation_PHP3_Summer_2024/resources/views/layouts/layout-user.blade.php
+++ Find_Accommodation_PHP3_Summer_2024/resources/views/layouts/layout-user.blade.php
@@ -408,7 +408,77 @@
         </footer>
     </div>
 
-
+    <script>
+        // Configs
+        let liveChatBaseUrl = document.location.protocol + '//' + 'livechat.fpt.ai/v36/src'
+        let LiveChatSocketUrl = 'livechat.fpt.ai:443'
+        let FptAppCode = '0ef59efeff9ae59af7e723a6c3e8cf80'
+        let FptAppName = 'Hỗ Trợ Trực Tuyến'
+        // Define custom styles
+        let CustomStyles = {
+            // header
+            headerBackground: 'linear-gradient(86.7deg, #3353a2ff 0.85%, #4949DCFF 98.94%)',
+            headerTextColor: '#FFFFFFFF',
+            headerLogoEnable: false,
+            headerLogoLink: 'https://chatbot-tools.fpt.ai/livechat-builder/img/Icon-fpt-ai.png',
+            headerText: 'Hỗ Trợ Trực Tuyến',
+            // main
+            primaryColor: '#4949DCFF',
+            secondaryColor: '#ecececff',
+            primaryTextColor: '#ffffffff',
+            secondaryTextColor: '#000000DE',
+            buttonColor: '#b4b4b4ff',
+            buttonTextColor: '#ffffffff',
+            bodyBackgroundEnable: false,
+            bodyBackgroundLink: '',
+            avatarBot: 'https://cdn.pixabay.com/photo/2016/06/13/17/30/mail-1454732_960_720.png',
+            sendMessagePlaceholder: 'Tin nhắn...',
+            // float button
+            floatButtonLogo: 'https://cdn.pixabay.com/photo/2016/06/13/17/30/mail-1454732_960_720.png',
+            floatButtonTooltip: 'Can I help you?',
+            floatButtonTooltipEnable: false,
+            // start screen
+            customerLogo: 'https://cdn.pixabay.com/photo/2016/06/13/17/30/mail-1454732_960_720.png',
+            customerWelcomeText: 'Vui lòng nhập tên',
+            customerButtonText: 'Bắt đầu',
+            prefixEnable: false,
+            prefixType: 'radio',
+            prefixOptions: ["Anh", "Chị"],
+            prefixPlaceholder: 'Danh xưng',
+            // custom css
+            css: ''
+        }
+        // Get bot code from url if FptAppCode is empty
+        if (!FptAppCode) {
+            let appCodeFromHash = window.location.hash.substr(1)
+            if (appCodeFromHash.length === 32) {
+                FptAppCode = appCodeFromHash
+            }
+        }
+        // Set Configs
+        let FptLiveChatConfigs = {
+            appName: FptAppName,
+            appCode: FptAppCode,
+            themes: '',
+            styles: CustomStyles
+        }
+        // Append Script
+        let FptLiveChatScript = document.createElement('script')
+        FptLiveChatScript.id = 'fpt_ai_livechat_script'
+        FptLiveChatScript.src = liveChatBaseUrl + '/static/fptai-livechat.js'
+        document.body.appendChild(FptLiveChatScript)
+        // Append Stylesheet
+        let FptLiveChatStyles = document.createElement('link')
+        FptLiveChatStyles.id = 'fpt_ai_livechat_script'
+        FptLiveChatStyles.rel = 'stylesheet'
+        FptLiveChatStyles.href = liveChatBaseUrl + '/static/fptai-livechat.css'
+        document.body.appendChild(FptLiveChatStyles)
+        // Init
+        FptLiveChatScript.onload = function() {
+            fpt_ai_render_chatbox(FptLiveChatConfigs, liveChatBaseUrl, LiveChatSocketUrl)
+        }
+    </script>
     @stack('scripts')
 </body>
+
 </html>
